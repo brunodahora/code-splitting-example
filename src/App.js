09027a7ee57dbfbeae690fbb38a5e47f848f6140
Bruno Vercelino da Hora <bruno.hora@pipefy.com>
2018-06-23 17:28:13 -0300

    Implemented code splitting

--- src/App.js
+++ src/App.js
@@ -1,47 +1,26 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Loadable from 'react-loadable';
+import ReactLoading from 'react-loading';
 
-const Home = () => (
-  <div>
-    <h2>Home</h2>
-  </div>
+const Loading = ({ type, color }) => (
+  <ReactLoading type={type} color={color} height={667} width={375} />
 );
 
-const About = () => (
-  <div>
-    <h2>About</h2>
-  </div>
-);
+const LoadableHome = Loadable({
+  loader: () => import('./components/Home'),
+  loading: Loading
+});
 
-const Topic = ({ match }) => (
-  <div>
-    <h3>{match.params.topicId}</h3>
-  </div>
-);
+const LoadableAbout = Loadable({
+  loader: () => import('./components/About'),
+  loading: Loading
+});
 
-const Topics = ({ match }) => (
-  <div>
-    <h2>Topics</h2>
-    <ul>
-      <li>
-        <Link to={`${match.url}/rendering`}>Rendering with React</Link>
-      </li>
-      <li>
-        <Link to={`${match.url}/components`}>Components</Link>
-      </li>
-      <li>
-        <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
-      </li>
-    </ul>
-
-    <Route path={`${match.path}/:topicId`} component={Topic} />
-    <Route
-      exact
-      path={match.path}
-      render={() => <h3>Please select a topic.</h3>}
-    />
-  </div>
-);
+const LoadableTopics = Loadable({
+  loader: () => import('./components/Topics'),
+  loading: Loading
+});
 
 const BasicExample = () => (
   <Router>
@@ -60,9 +39,9 @@ const BasicExample = () => (
 
       <hr />
 
-      <Route exact path="/" component={Home} />
-      <Route path="/about" component={About} />
-      <Route path="/topics" component={Topics} />
+      <Route exact path="/" component={LoadableHome} />
+      <Route path="/about" component={LoadableAbout} />
+      <Route path="/topics" component={LoadableTopics} />
     </div>
   </Router>
 );
